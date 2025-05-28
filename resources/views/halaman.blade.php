edffee3b5d64b055b053ac3befb74484d02309c9
farcodee <fartod1111@gmail.com>
2025-05-28 13:03:40 +0700

    nav

--- resources/views/halaman.blade.php
+++ resources/views/halaman.blade.php
@@ -5,9 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+     
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <!-- Container wrapper -->
   <div class="container-fluid">
     <!-- Toggle button -->
